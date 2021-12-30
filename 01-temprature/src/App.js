0385adacb9030eb0ef17b6f2c022864cecdfbb87
leventsamlioglu <leventsamlioglu@icloud.com>
2021-12-30 19:27:35 +0100

    seperate concerns

--- 01-temprature/src/App.js
+++ 01-temprature/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
-
-const scaleNames = {
-  c: "Celsius",
-  f: "Fahrenheit",
-};
+import TemperatureInput from "./Temp";
+import BoilingVerdict from "./Boil";
 
 function toCelsius(fahrenheit) {
   return ((fahrenheit - 32) * 5) / 9;
@@ -22,37 +19,6 @@ function tryConvert(temperature, convert) {
   const rounded = Math.round(output * 1000) / 1000;
   return rounded.toString();
 }
-
-function BoilingVerdict(props) {
-  if (!props.celsius) return "";
-  if (props.celsius >= 100) {
-    return <p>The water would boil.</p>;
-  }
-  return <p>The water would not boil.</p>;
-}
-
-class TemperatureInput extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onTemperatureChange(e.target.value);
-  }
-
-  render() {
-    const temperature = this.props.temperature;
-    const scale = this.props.scale;
-    return (
-      <fieldset>
-        <legend>Enter temperature in {scaleNames[scale]}:</legend>
-        <input value={temperature} onChange={this.handleChange} />
-      </fieldset>
-    );
-  }
-}
-
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
---
+++ 01-temprature/src/Boil.js
@@ -0,0 +1,9 @@
+function BoilingVerdict(props) {
+  if (!props.celsius) return "";
+  if (props.celsius >= 100) {
+    return <p>The water would boil.</p>;
+  }
+  return <p>The water would not boil.</p>;
+}
+
+export default BoilingVerdict;
