af2a6b3d2273f3454b4bc8b8deb3fd9751fb1b40
leventsamlioglu <leventsamlioglu@icloud.com>
2022-01-05 14:53:18 +0100

    add Axios API

--- 02-movie-list/src/App.js
+++ 02-movie-list/src/App.js
@@ -1,22 +1,39 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
+import axios from "axios";
 class App extends React.Component {
   state = {
     movies: [],
     searchText: "",
   };
 
+  // FETCH API
+  // async componentDidMount() {
+  //   const basURL = "http://localhost:3001/movies";
+  //   const response = await fetch(basURL);
+  //   const data = await response.json();
+  //   this.setState({ movies: data });
+  // }
+
+  // deleteMovie = async (movie) => {
+  //   const basURL = `http://localhost:3001/movies/${movie.id}`;
+  //   await fetch(basURL, { method: "DELETE" });
+  //   const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
+
+  //   this.setState((state) => ({ movies: newMovieList }));
+  // };
+
+  // AXIOS API
   async componentDidMount() {
     const basURL = "http://localhost:3001/movies";
-    const response = await fetch(basURL);
-    const data = await response.json();
-    this.setState({ movies: data });
+    const response = await axios.get(basURL);
+    this.setState({ movies: response.data });
   }
 
   deleteMovie = async (movie) => {
     const basURL = `http://localhost:3001/movies/${movie.id}`;
-    await fetch(basURL, { method: "DELETE" });
+    await axios.delete(basURL);
     const newMovieList = this.state.movies.filter((m) => m.id !== movie.id);
 
     this.setState((state) => ({ movies: newMovieList }));
