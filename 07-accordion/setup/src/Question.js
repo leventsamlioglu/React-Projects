4ca20d7fd26e47a2e88887e4287a16eda54f32d0
leventsamlioglu <leventsamlioglu@icloud.com>
2022-01-26 17:26:22 +0100

    finish project

--- 07-accordion/setup/src/Question.js
+++ 07-accordion/setup/src/Question.js
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+const Question = ({ title, info }) => {
+  const [state, setState] = useState(false);
+
+  return (
+    <article className='question'>
+      <header>
+        <h4>{title}</h4>
+        <button className='btn' onClick={() => setState(!state)}>
+          {state ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        </button>
+      </header>
+      {state && <p>{info}</p>}
+    </article>
+  );
 };
 
 export default Question;
