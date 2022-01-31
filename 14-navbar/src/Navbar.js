c21464b70f7d3a9c3a7f0f71be6e658d3f12695e
leventsamlioglu <leventsamlioglu@icloud.com>
2022-01-31 13:46:22 +0100

    finish project

--- 14-navbar/src/Navbar.js
+++ 14-navbar/src/Navbar.js
@@ -4,43 +4,53 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [showLinks]);
   return (
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
           <img src={logo} alt='logo' />
-          <button className='nav-toggle'>
+          <button
+            className='nav-toggle'
+            onClick={() => setShowLinks(!showLinks)}
+          >
             <FaBars />
           </button>
         </div>
-        <div className='links-container show-container'>
-          <ul className='links'>
-            <li>
-              <a href='#'>home</a>
-            </li>
-            <li>
-              <a href='#'>about</a>
-            </li>
-            <li>
-              <a href='#'>contact</a>
-            </li>
-            <li>
-              <a href='#'>products</a>
-            </li>
+
+        <div className='links-container' ref={linksContainerRef}>
+          <ul className='links' ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
           </ul>
         </div>
+
         <ul className='social-icons'>
-          <li>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-            <a href='https://www.twitter.com'>
-              <FaTwitter />
-            </a>
-          </li>
+          {social.map((link) => {
+            const { id, url, icon } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </nav>
