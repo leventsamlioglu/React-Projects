0bf5f6616f093db8bb99af10a1579f198a5da77a
leventsamlioglu <leventsamlioglu@icloud.com>
2022-05-12 15:40:35 +0200

    add index.js

--- 20-movie-db/src/index.js
+++ 20-movie-db/src/index.js
@@ -1,7 +1,14 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import { AppProvider } from "./context";
+import { BrowserRouter as Router } from "react-router-dom";
+ReactDOM.render(
+  <AppProvider>
+    <Router>
+      <App />
+    </Router>
+  </AppProvider>,
+  document.getElementById("root")
+);
